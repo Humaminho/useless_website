65c464d2c5f9270c440e4f6a9e068206f9358155
humam <hu666mam@gmail.com>
2023-05-03 16:41:10 +0100

    palettes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import lightPalette from '../data/lightPalette.tsx';
 import '../css/App.css';
 
 function App() {
-	return <div className='app'>Hello</div>;
+
+	return <div className="app">Hello</div>;
 }
 
 export default App;
---
+++ src/data/lightPalette.tsx
@@ -0,0 +1,32 @@
+const lightPalette = [
+	{
+		primary: '#ffffff',
+		secondary: '#F0625F',
+		emph: '#b80c09',
+		text: '#000000',
+		extra: '#6f8695',
+	},
+	{
+		primary: '#f1faee',
+		secondary: '#a8dadc',
+		emph: '#457b9d',
+		text: '#152733',
+		extra: '#e63946',
+	},
+	{
+		primary: '#fbf5f3',
+		secondary: '#8daa9d',
+		emph: '#c1292e',
+		text: '#0E1011',
+		extra: '#32373b',
+	},
+	{
+		primary: '#d0db97',
+		secondary: '#69b578',
+		emph: '#3a7d44',
+		text: '#0D1B12',
+		extra: '#333745',
+	},
+];
+
+export default lightPalette;
